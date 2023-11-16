0ceb8642e70a39348a3993eaab7700b4f1940c42
Gokulkrishnanrg2004 <125328166+Gokulkrishnanrg2004@users.noreply.github.com>
2023-11-16 22:53:34 +0530

    Create Coding_ninjas_find_case.cpp

---
+++ Dya 9/Coding_ninjas_find_case.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    char a;
+    cin>>a;
+    if(a>='A' && a<='Z')
+    {
+        cout<<"1"<<endl;
+    }
+    else if(a>='a' && a<='z')
+    {
+        cout<<"0"<<endl;
+    }
+    else
+    {
+        cout<<"-1"<<endl;
+    }
+}
